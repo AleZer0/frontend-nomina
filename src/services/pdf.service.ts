d2328b964e4d7c11d4fab2bbac4d9f1b81eb0e0a
Alexis Balaam Díaz Rodríguez <a7exis2003@outlook.com>
2025-03-21 16:32:03 -0600

    change url api

--- src/services/pdf.service.ts
+++ src/services/pdf.service.ts
@@ -1,14 +1,14 @@
 import { WeeklyReportData } from '../types';
 export class PDF {
     static previewPayrollPDF = (folio: number) => {
-        const backendURL = 'https://app-nomina-141e425e046a.herokuapp.com/api';
+        const backendURL = 'https://transportesgodoy-nomina.xrom.cc/api';
         const pdfUrl = `${backendURL}/pdf/nomina/${folio}`;
 
         window.open(pdfUrl, '_blank');
     };
 
     static previewWeeklyReportsPDF = (year: number, data: WeeklyReportData) => {
-        const backendURL = 'https://app-nomina-141e425e046a.herokuapp.com/api';
+        const backendURL = 'https://transportesgodoy-nomina.xrom.cc/api';
         const pdfUrl = `${backendURL}/pdf/reporte_semanal/${year}?semana=${data.semana}&empleados_pagados=${data.empleados_pagados}&total_sueldos=${data.total_sueldos}&total_vacaciones=${data.total_vacaciones}&total_aguinaldos=${data.total_aguinaldos}&total_finiquitos=${data.total_finiquitos}&total_prestamos=${data.total_prestamos}&total_infonavit=${data.total_infonavit}&total_pension_alimenticia=${data.total_pension_alimenticia}&total_pago_horas_extras=${data.total_pago_horas_extras}&total_maniobras=${data.total_maniobras}&total_otros=${data.total_otros}&total_neto=${data.total_neto}`;
 
         window.open(pdfUrl, '_blank');
